d05fdfe0a0cfc8c3dea71a5ff6c5d60773935281
Harini Suresh <hsuresh@mit.edu>
2019-07-21 15:10:45 -0400

    base

---
+++ src/webWorker/index.js
@@ -0,0 +1,18 @@
+import { registerTaskHandler } from './webWorker.js';
+import decodeTask from './decodeTask/decodeTask.js';
+import { default as version } from '../version.js';
+
+// register our task
+registerTaskHandler(decodeTask);
+
+const cornerstoneWADOImageLoaderWebWorker = {
+  registerTaskHandler,
+  version
+};
+
+export {
+  registerTaskHandler,
+  version
+};
+
+export default cornerstoneWADOImageLoaderWebWorker;
